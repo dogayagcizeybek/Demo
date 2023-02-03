daeece10d2bbf3388da62232f9dd0388f02af343
Doğa Yağcızeybek <dogayagcizeybek@gmail.com>
2023-02-03 23:16:08 +0300

    commit11

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -9,7 +9,4 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World2!"}
-    response2 = client.get("/people")
-    assert response2.status_code == 200
-    item_dict = json.loads(response2.json())
-    assert len(item_dict) > 0
+
