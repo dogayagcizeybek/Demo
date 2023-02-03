b47b39987e7d493fd2883ae8a33aed4f8b6eaab1
Doğa Yağcızeybek <dogayagcizeybek@gmail.com>
2023-02-03 15:13:02 +0300

    commit 10

--- app/local_settings.py
+++ app/local_settings.py
@@ -1,5 +1,5 @@
 postgresql = {"pguser": "postgres",
-              "pgpasswd":"",
+              "pgpasswd":"123",
               "pghost":"localhost",
                   "pgport":5432,
                       "pgdb":"postgres"}
